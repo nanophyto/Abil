13b23a5ade3d9358e26d5aa9cbfb53d7b95a3811
nanophyto <coralscaper@gmail.com>
2024-07-23 16:29:22 +0100

    added post to test

--- tests/test.py
+++ tests/test.py
@@ -8,6 +8,7 @@ import pandas as pd
 from abil.tune import tune
 from abil.functions import example_data, upsample
 from abil.predict import predict
+from abil.post import post
 
 
 class TestRegressors(unittest.TestCase):
@@ -99,6 +100,41 @@ class TestRegressors(unittest.TestCase):
         m = predict(X_train, y, X_predict, model_config)
         m.make_prediction()
 
+    def test_post_ensemble(self):
+
+        self.test_predict_ensemble()
+
+        with open('./examples/configuration/regressor.yml', 'r') as f:
+            model_config = load(f, Loader=Loader)
+
+        model_config['hpc']==False
+
+        predictors = model_config['predictors']
+        d = pd.read_csv("./examples/data/training.csv")
+        d.dropna(subset='FID', inplace=True)
+        X_train = d[predictors]
+        X_predict = pd.read_csv("./examples/data/prediction.csv")
+        X_predict.set_index(["time", "depth", "lat", "lon"], inplace=True)
+        X_predict = X_predict[X_train.columns]
+
+        m = post(model_config)
+        m.merge_performance(model="ens")
+        m.merge_performance(model="xgb", configuration= "reg")
+        m.merge_performance(model="rf", configuration= "reg")
+        m.merge_performance(model="knn", configuration= "reg")
+
+        m.merge_parameters(model="rf")
+        m.merge_parameters(model="xgb")
+        m.merge_parameters(model="knn")
+
+        m.total()
+
+        m.merge_env(X_predict)
+
+        m.export_ds("test")
+        m.export_csv("test")
+
+
 if __name__ == '__main__':
     # Create a test suite combining all test cases in order
     suite = unittest.TestSuite()
@@ -108,6 +144,7 @@ if __name__ == '__main__':
     suite.addTest(TestRegressors('test_tune_xgb'))
     suite.addTest(TestRegressors('test_tune_knn'))
     suite.addTest(TestRegressors('test_predict_ensemble'))
+    suite.addTest(TestRegressors('test_post_ensemble'))
 
     # Run the test suite
     runner = unittest.TextTestRunner()
