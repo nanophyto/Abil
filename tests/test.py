b377824fec71ab09919b1c2f1ef594b712ffb4ad
Nicola Wiseman <nicola.wiseman@bristol.ac.uk>
2024-08-02 13:11:10 +0100

    ignore gamma_conformity

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,7 @@ from yaml import load
 from yaml import CLoader as Loader
 import pandas as pd
 from abil.tune import tune
-from abil.functions import upsample, OffsetGammaConformityScore
+from abil.functions import upsample#, OffsetGammaConformityScore
 from abil.predict import predict
 from abil.post import post
 
@@ -43,28 +43,33 @@ class TestRegressors(unittest.TestCase):
         m = predict(self.X_train, self.y, self.X_predict, self.model_config)
         m.make_prediction(prediction_inference=True)
 
-        m = post(self.model_config)
-        m.merge_performance(model="ens") 
-        m.merge_performance(model="xgb")
-        m.merge_performance(model="rf")
-        m.merge_performance(model="knn")
+        def do_post(pi):
+            m = post(self.model_config, pi=pi)
+            m.merge_performance(model="ens") 
+            m.merge_performance(model="xgb")
+            m.merge_performance(model="rf")
+            m.merge_performance(model="knn")
 
-        m.merge_parameters(model="rf")
-        m.merge_parameters(model="xgb")
-        m.merge_parameters(model="knn")
+            m.merge_parameters(model="rf")
+            m.merge_parameters(model="xgb")
+            m.merge_parameters(model="knn")
 
-        m.total()
+            m.total()
 
-        m.merge_env(self.X_predict)
+            m.merge_env(self.X_predict)
 
-        m.export_ds("test")
-        m.export_csv("test")
+            m.export_ds("test")
+            m.export_csv("test")
 
-        targets = ['Emiliania huxleyi', 'total']
-        vol_conversion = 1e3 #L-1 to m-3
-        integ = m.integration(m, vol_conversion=vol_conversion)
-        integ.integrated_totals(targets)
-        integ.integrated_totals(targets, subset_depth=100)
+            targets = ['Emiliania huxleyi','total']
+            vol_conversion = 1e3 #L-1 to m-3
+            integ = m.integration(m, vol_conversion=vol_conversion)
+            integ.integrated_totals(targets)
+            integ.integrated_totals(targets, subset_depth=100)
+
+        do_post(pi="50")
+        do_post(pi="95_UL")
+        do_post(pi="95_LL")
 
 
 
@@ -91,7 +96,7 @@ class Test2Phase(unittest.TestCase):
         X_predict.set_index(["time", "depth", "lat", "lon"], inplace=True)
         self.X_predict = X_predict[predictors]
 
-    def test_ensemble(self):
+    def test_post_ensemble(self):
 
 
         m = tune(self.X_train, self.y, self.model_config)
@@ -104,28 +109,33 @@ class Test2Phase(unittest.TestCase):
         m.make_prediction(prediction_inference=True)
 
 
-        m = post(self.model_config)
-        m.merge_performance(model="ens") 
-        m.merge_performance(model="xgb")
-        m.merge_performance(model="rf")
-        m.merge_performance(model="knn")
+        def do_post(pi):
+            m = post(self.model_config, pi=pi)
+            m.merge_performance(model="ens") 
+            m.merge_performance(model="xgb")
+            m.merge_performance(model="rf")
+            m.merge_performance(model="knn")
 
-        m.merge_parameters(model="rf")
-        m.merge_parameters(model="xgb")
-        m.merge_parameters(model="knn")
+            m.merge_parameters(model="rf")
+            m.merge_parameters(model="xgb")
+            m.merge_parameters(model="knn")
 
-        m.total()
+            m.total()
 
-        m.merge_env(self.X_predict)
+            m.merge_env(self.X_predict)
 
-        m.export_ds("test")
-        m.export_csv("test")
+            m.export_ds("test")
+            m.export_csv("test")
 
-        targets = ['Emiliania huxleyi', 'total']
-        vol_conversion = 1e3 #L-1 to m-3
-        integ = m.integration(m, vol_conversion=vol_conversion)
-        integ.integrated_totals(targets)
-        integ.integrated_totals(targets, subset_depth=100)
+            targets = ['Emiliania huxleyi','total']
+            vol_conversion = 1e3 #L-1 to m-3
+            integ = m.integration(m, vol_conversion=vol_conversion)
+            integ.integrated_totals(targets)
+            integ.integrated_totals(targets, subset_depth=100)
+
+        do_post(pi="50")
+        do_post(pi="95_UL")
+        do_post(pi="95_LL")
 
 
 
@@ -152,7 +162,64 @@ class TestClassifiers(unittest.TestCase):
         X_predict.set_index(["time", "depth", "lat", "lon"], inplace=True)
         self.X_predict = X_predict[predictors]
 
-    def test_ensemble(self):
+    def test_post_ensemble(self):
+   
+        m = tune(self.X_train, self.y, self.model_config)
+        m.train(model="rf", classifier=True)
+        m.train(model="xgb", classifier=True)
+        m.train(model="knn", classifier=True)
+
+        m = predict(self.X_train, self.y, self.X_predict, self.model_config)
+        m.make_prediction(prediction_inference=True)
+
+        def do_post(pi):
+            m = post(self.model_config, pi=pi)
+            m.merge_performance(model="ens") 
+            m.merge_performance(model="xgb")
+            m.merge_performance(model="rf")
+            m.merge_performance(model="knn")
+
+            m.merge_parameters(model="rf")
+            m.merge_parameters(model="xgb")
+            m.merge_parameters(model="knn")
+
+            m.total()
+
+            m.merge_env(self.X_predict)
+
+            m.export_ds("test")
+            m.export_csv("test")
+
+        do_post(pi="50")
+        do_post(pi="95_UL")
+        do_post(pi="95_LL")
+
+
+
+class TestClassifiers(unittest.TestCase):
+
+    def setUp(self):
+        self.workspace = os.getenv('GITHUB_WORKSPACE', '.')
+        with open(self.workspace +'/tests/classifier.yml', 'r') as f:
+            self.model_config = load(f, Loader=Loader)
+
+        self.model_config['local_root'] = self.workspace # yaml_path
+        predictors = self.model_config['predictors']
+        d = pd.read_csv(self.model_config['local_root'] + self.model_config['training'])
+        target =  "Emiliania huxleyi"
+        d[target] = d[target].fillna(0)
+        d = upsample(d, target, ratio=10)
+        d = d.dropna(subset=[target])
+        d = d.dropna(subset=predictors)
+
+        self.X_train = d[predictors]
+        self.y = d[target]
+
+        X_predict = pd.read_csv(self.model_config['local_root'] + self.model_config['prediction'])
+        X_predict.set_index(["time", "depth", "lat", "lon"], inplace=True)
+        self.X_predict = X_predict[predictors]
+
+    def test_post_ensemble(self):
    
         m = tune(self.X_train, self.y, self.model_config)
         m.train(model="rf", classifier=True)
@@ -221,7 +288,7 @@ class TestGammaOffset(unittest.TestCase):
 if __name__ == '__main__':
     # Create a test suite combining all test cases in order
     suite = unittest.TestSuite()
-    suite.addTest(TestClassifiers('test_ensemble'))
+    suite.addTest(TestClassifiers('test_post_ensemble'))
     suite.addTest(TestRegressors('test_post_ensemble'))
     suite.addTest(Test2Phase('test_post_ensemble'))
     runner = unittest.TextTestRunner()
