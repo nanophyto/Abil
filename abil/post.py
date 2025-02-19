ef958a718e8aba2052caecc2085c8443a10b6c34
nanophyto <coralscaper@gmail.com>
2025-02-19 10:16:53 +0000

    fix post rendering

--- abil/post.py
+++ abil/post.py
@@ -10,10 +10,9 @@ from skbio.diversity.alpha import shannon
 # from . import analyze #it seems this does not work on my machine?
 
 if 'site-packages' in __file__ or os.getenv('TESTING') == 'true':
-    from abil.analyze import area_of_applicability
-else:
     from analyze import area_of_applicability
-
+else:
+    from abil.analyze import area_of_applicability
 
 class post:
     """
