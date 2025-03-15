5c120ade2789567a2af83f3240d25cad3a8dd4bb
nanophyto <coralscaper@gmail.com>
2025-03-15 12:49:16 +0000

    restore full test

--- tests/test.py
+++ tests/test.py
@@ -59,8 +59,8 @@ class TestRegressors(unittest.TestCase):
             integ.integrated_totals(targets, monthly=True)
 
         do_post(statistic="mean")
-#        do_post(statistic="ci95_UL")
-#        do_post(statistic="ci95_LL")
+        do_post(statistic="ci95_UL")
+        do_post(statistic="ci95_LL")
 
 
 
@@ -113,13 +113,13 @@ class Test2Phase(unittest.TestCase):
             integ.integrated_totals(targets)
 
         do_post(statistic="mean")
-#        do_post(statistic="ci95_UL")
-#        do_post(statistic="ci95_LL")
+        do_post(statistic="ci95_UL")
+        do_post(statistic="ci95_LL")
 
 if __name__ == '__main__':
     # Create a test suite combining all test cases in order
     suite = unittest.TestSuite()
-#    suite.addTest(TestRegressors('test_post_ensemble'))
+    suite.addTest(TestRegressors('test_post_ensemble'))
     suite.addTest(Test2Phase('test_post_ensemble'))
     runner = unittest.TextTestRunner()
     runner.run(suite)
