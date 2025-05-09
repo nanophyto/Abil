f78dd2ef339e646b77e3fc41829cc0966299706b
Nicola Wiseman <nicola.wiseman@bristol.ac.uk>
2025-05-09 15:20:03 +0100

    add target_subset for estimate_applicability(), merge_obs(). and integrate(). Add pseudo-zero mask for estimate_applicability. Move some functions within do_post to if statements to only run when statistic=mean as they are not needed for the CI datasets.

--- studies/wiseman2024/hpc_post.py
+++ studies/wiseman2024/hpc_post.py
@@ -24,6 +24,7 @@ model_config['hpc'] = True
 targets = pd.read_csv(os.path.join(root,model_config['targets']))
 target =  targets['Target'][0]
 targets = targets['Target'].values
+target_subset = targets[:2] # subset for estimate_applicability and merge_obs
 d = pd.read_csv(os.path.join(root,model_config['training']))
 predictors = model_config['predictors']
 d = d.dropna(subset=predictors)
@@ -36,20 +37,37 @@ X_predict = X_predict.dropna()
 y = d[target]
 X_train = d[predictors]
 
+# Create a mask where y is a pseudo-zero
+mask = y != 0
+y = y[mask]
+X_train = X_train[mask]
+
 def do_post(statistic):
     m = post(X_train, y, X_predict, model_config, statistic)
-    #m.estimate_applicability()
-    #m.merge_env(X_predict)
-    #m.merge_obs(current_date,targets)
     
+    if statistic == "mean":
+        print('begin estimate_applicability')
+        m.estimate_applicability(target_subset)
+
+        print('begin merge_obs')
+        m.merge_obs(current_date,target_subset)
+    
+    print('begin process_resampled_runs')
     m.process_resampled_runs()
 
+    print('begin export_ds')
     m.export_ds(current_date)
 
+    print('begin integration')
     magnitude_conversion = 1e-21
     molar_mass = 12.01
     integ = m.integration(m, magnitude_conversion=magnitude_conversion,molar_mass=molar_mass,rate=True)
-    integ.integrated_totals(targets)
+    if statistic == "mean":
+        integ.integrated_totals(targets)
+    else:
+        integ.integrated_totals(target_subset)
+
+    print('do_post for: ', statistic, ' complete')
 
 do_post(statistic="mean")
 do_post(statistic="ci95_UL")
